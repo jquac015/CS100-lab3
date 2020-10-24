f4a3e595e7265940d53ff6902894e718ee96ba01
Jackie-Qin <jackieqin221@gmail.com>
2020-10-23 17:45:03 -0700

    Fixing #2

--- test.cpp
+++ test.cpp
@@ -1,38 +1,34 @@
 #include "c-echo.h"
+#include "c-count.h"
 
 #include "gtest/gtest.h"
-//Hellow World Test
+
 TEST(EchoTest, HelloWorld) {
-    char* test_val[3]; test_val[0] = "./c-echo"; test_val[1] = "hello"; test_val[2] = "world";
+    char* test_val[3]; test_val[0] = "./c-echo-count"; test_val[1] = "hello"; test_val[2] = "world";
     EXPECT_EQ("hello world", echo(3,test_val));
 }
-//Empty String Test
+
 TEST(EchoTest, EmptyString) {
-    char* test_val[1]; test_val[0] = "./c-echo";
-    EXPECT_EQ("", echo(1,test_val));
+    char* test_val[1]; test_val[0] = "./c-echo-count";
+    EXPECT_EQ("", echo(1, test_val));
 }
-//Print numbers
-TEST(EchoTest, Numbers) {
-    char* test_val[3]; test_val[0] = "./c-echo";test_val[1] = "12345"; test_val[2] = "67890";
-    EXPECT_EQ("12345 67890", echo(3,test_val));
+
+TEST(CountTest, HelloWorld) {
+    std::string test_str = "hello world";
+    EXPECT_EQ(2, count(test_str));
 }
-//Print Empty First Input
-TEST(EchoTest, EmptyFirst) {
-    char* test_val[3]; test_val[0] = "./c-echo";test_val[1] = ""; test_val[2] = "Testing";
-    EXPECT_EQ(" Testing", echo(3,test_val));
+
+TEST(CountTest, EmptyString) {
+    std::string test_str = "";
+    EXPECT_EQ(0, count(test_str));
 }
-//Print Empty Second Input
-TEST(EchoTest, EmptySecond) {
-    char* test_val[3]; test_val[0] = "./c-echo";test_val[1] = "Testing"; test_val[2] = "";
-    EXPECT_EQ("Testing ", echo(3,test_val));
-}
-//1 Inputs
-TEST(EchoTest, OneInput) {
-    char* test_val[2]; test_val[0] = "./c-echo";test_val[1] = "Test";
-    EXPECT_EQ("Test", echo(2,test_val));
+
+TEST(CountTest, ManySpaces) {
+    std::string test_str = "   this   string has     weird   spacing";
+    EXPECT_EQ(5, count(test_str));
 }
 
 int main(int argc, char **argv) {
-  ::testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
